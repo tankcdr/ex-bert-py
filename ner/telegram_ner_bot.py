ee6547bc3bc6c21aadd19930b0f3f2e10d2ca7e5
Christopher Madison <chris_madison@bellsouth.net>
2025-01-15 12:49:51 -0500

    testing simple telegram chatbot

--- ner/telegram_ner_bot.py
+++ ner/telegram_ner_bot.py
@@ -14,28 +14,41 @@ from dl_translate import TranslationModel
 # Configuration and setup
 ######################################################################
 
-# # Get the absolute path of the directory where the script is located
-# script_dir = os.path.dirname(os.path.abspath(__file__))
-# # Construct the full path to the .env file
-# env_path = os.path.join(script_dir, '../.env')
-# # Load environment variables from .env file
-# load_dotenv(dotenv_path=env_path)
-#
-# # Get the Telegram bot API key and initialize the bot
-# bot_api_key = os.getenv("TELEGRAM_BOT_KEY")
-# bot = telebot.TeleBot(bot_api_key, parse_mode=None)
+# Get the absolute path of the directory where the script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Construct the full path to the .env file
+env_path = os.path.join(script_dir, '../.env')
+# Load environment variables from .env file
+load_dotenv(dotenv_path=env_path)
+
+# Get the Telegram bot API key and initialize the bot
+bot_api_key = os.getenv("TELEGRAM_BOT_KEY")
+bot = telebot.TeleBot(bot_api_key, parse_mode=None)
+
+# setup the pipeline
+ner = pipeline("ner", aggregation_strategy="simple")
 
 
 ######################################################################
 # Implementation
 ######################################################################
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    """Handles the incoming messages from the user.
+       Simple named entity recognition using the transformers pipeline.
+    """
+    entities = ner(message.text)
+    person = "not found"
+    location = "not found"
 
-def main():
-    ner = pipeline("ner", model="blaze999/Medical-NER",
-                   tokenizer="blaze999/Medical-NER", aggregation_strategy="simple")
+    for entity in entities:
+        if entity["entity_group"] == "PER":
+            person = entity["word"]
+        elif entity["entity_group"] == "LOC":
+            location = entity["word"]
 
-    print(ner("Take 1mg of Tylenol every 12 hours for 5 days."))
+    bot.reply_to(message, f"Hi {
+                 person}, your product will be delivered to {location}.")
 
 
-if __name__ == "__main__":
-    main()
+bot.infinity_polling()
